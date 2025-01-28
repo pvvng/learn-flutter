6743920b319ff4d49ac585f79d3c89446fa5d166
맥동우 <t6u80o@naver.com>
2025-01-28 14:28:03 +0900

    vscode 세팅

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 // 모든 것은 위젯이다. 블록처럼 위젯에 위젯을 쌓아가며 앱을 만드는 것.
 // class를 위젯으로 만들기 위해서는 상속이 필요함
 class MyApp extends StatelessWidget {
-  // 이거 해줘야 함
+  // 상수화 시켜서 컴파일하기 편하게 만들기
   const MyApp({super.key});
 
   // 모든 위젯은 build 메서드를 사용해야 한다
